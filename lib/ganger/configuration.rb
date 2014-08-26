ab3678ac286322497c3fa6040d72df953b87f61f
Andy Sykes <github@tinycat.co.uk>
2014-08-26 15:20:04 +0100

    Merge docker_cmd - everything can now be specified in a single array

--- lib/ganger/configuration.rb
+++ lib/ganger/configuration.rb
@@ -1,15 +1,11 @@
 module Ganger
   class Configuration
         
-    attr_accessor :docker_daemons, :docker_image, :docker_cmd, :docker_args, :docker_expose
+    attr_accessor :docker_daemons, :docker_image, :docker_cmd, :docker_expose
     attr_accessor :proxy_listen_port, :service_timeout, :service_retry
     
     def initialize
       @docker_daemons = ['unix:///var/run/docker.sock']
-    end
-    
-    def docker_cmd_and_args
-      [@docker_cmd] + @docker_args
     end
     
     def inspect
@@ -21,7 +17,6 @@ module Ganger
         docker_daemons: @docker_daemons,
         docker_image: @docker_image,
         docker_cmd: @docker_cmd,
-        docker_args: @docker_args,
         docker_expose: @docker_expose,
         proxy_listen_port: @proxy_listen_port
       }.inspect
