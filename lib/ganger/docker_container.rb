6cda1c3b7ee9d5ea6f9e352ea4bd08c6646744d2
Andy Sykes <github@tinycat.co.uk>
2014-08-29 14:27:11 +0100

    Protection from faulty container launches
    Docker seems to have a bug where it'll fail to launch a container
    because it's reusing an ID. This commit makes Ganger dispose
    of a container if it creates one like this, and create a new
    one to service the connection.

--- lib/ganger/docker_container.rb
+++ lib/ganger/docker_container.rb
@@ -12,7 +12,7 @@ module Ganger
       
       # Figure out host and port for the service in this container
       @service_host = URI.parse(docker_uri).host
-      poll_for_service_port
+      get_service_port
       
       info "Service host is: #{@service_host}; service port is #{@service_port}"
       
@@ -30,23 +30,18 @@ module Ganger
     
     private
     
-    def poll_for_service_port
-      30.times do |count|
-        if @container.json["NetworkSettings"]["Ports"].nil? ||
-           @container.json["NetworkSettings"]["Ports"][Ganger.configuration.docker_expose].nil? ||
-           @container.json["NetworkSettings"]["Ports"][Ganger.configuration.docker_expose].empty? ||
-           @container.json["NetworkSettings"]["Ports"][Ganger.configuration.docker_expose].first.nil? ||
-           @container.json["NetworkSettings"]["Ports"][Ganger.configuration.docker_expose].first["HostPort"].nil?
-          # It can take a while for Docker to assign a service port if the daemon is under load
-          info "Container doesn't have a service port; sleeping and polling"
-          sleep 1
-        else
-          @service_port = @container.json["NetworkSettings"]["Ports"][Ganger.configuration.docker_expose].first["HostPort"]
-          info "Obtained service port: #{@service_port}"
-          break
-        end
+    def get_service_port
+      if @container.json["NetworkSettings"]["Ports"].nil? ||
+         @container.json["NetworkSettings"]["Ports"][Ganger.configuration.docker_expose].nil? ||
+         @container.json["NetworkSettings"]["Ports"][Ganger.configuration.docker_expose].empty? ||
+         @container.json["NetworkSettings"]["Ports"][Ganger.configuration.docker_expose].first.nil? ||
+         @container.json["NetworkSettings"]["Ports"][Ganger.configuration.docker_expose].first["HostPort"].nil?
+        @service_port = nil
+        info "No service port found - container is broken"
+      else
+        @service_port = @container.json["NetworkSettings"]["Ports"][Ganger.configuration.docker_expose].first["HostPort"]
+        info "Obtained service port: #{@service_port}"
       end
-      fatal "Couldn't get a service port for this container" if @service_port.nil?
     end
     
     def info(msg)
--- lib/ganger/docker_server.rb
+++ lib/ganger/docker_server.rb
@@ -9,8 +9,22 @@ module Ganger
       @containers = []
       @image_pulled = false
     end
-    
+
     def launch_container
+      loop do 
+        container = create_container
+        if container.service_port.nil?
+          warn "Got a broken container; disposing and recreating"
+          container.dispose
+          sleep 5
+        else
+          @containers << container
+          return container
+        end
+      end
+    end
+    
+    def create_container
       pull_image unless @image_pulled
       container_opts = {
         'Image' => Ganger.configuration.docker_image,
@@ -24,9 +38,7 @@ module Ganger
           Ganger.configuration.docker_expose => [{"HostPort" => ""}]
         }
       })
-      internal_container = DockerContainer.new(container, @docker_url)
-      @containers << internal_container
-      internal_container
+      DockerContainer.new(container, @docker_url)
     end
     
     def kill_containers
